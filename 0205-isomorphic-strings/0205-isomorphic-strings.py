29c964ee241c48a72d23d7a01021f27dea786e59
cck99 <161817893+cck99@users.noreply.github.com>
2024-08-15 23:02:38 +0800

    Update 0205-isomorphic-strings.py

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -18,8 +18,6 @@ class Solution:
         
         return True 
         '''
-        class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
         s_to_t = {}
         t_to_s = {}
 
