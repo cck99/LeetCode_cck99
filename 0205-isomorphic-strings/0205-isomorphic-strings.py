905803001db44ae81fb9c3e2900d10a2c5446950
cck99 <161817893+cck99@users.noreply.github.com>
2024-08-15 22:58:21 +0800

    Time: 40 ms (70.53%), Space: 16.7 MB (35.46%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,18 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        indexS = [0] * 200 # Stores index of characters in string s
-        indexT = [0] * 200 # Stores index of characters in string t
+        indexS = [0] * 200
+        indexT = [0] * 200
         
-        length = len(s) # Get the length of both strings
-        
-        if length != len(t): # If the lengths of the two strings are different, they can't be isomorphic
+        length = len(s)
+
+        if length != len(t): 
             return False
         
-        for i in range(length): # Iterate through each character of the strings
-            if indexS[ord(s[i])] != indexT[ord(t[i])]: # Check if the index of the current character in string s is different from the index of the corresponding character in string t
-                return False # If different, strings are not isomorphic
+        for i in range(length): 
+            if indexS[ord(s[i])] != indexT[ord(t[i])]: 
+                return False
             
-            indexS[ord(s[i])] = i + 1 # updating position of current character
+            indexS[ord(s[i])] = i + 1
             indexT[ord(t[i])] = i + 1
         
-        return True # If the loop completes without returning false, strings are isomorphic
+        return True 
