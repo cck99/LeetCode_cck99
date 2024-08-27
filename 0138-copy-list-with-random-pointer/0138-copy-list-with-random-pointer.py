2c90138bd6ba65dd6065528757172ca3525d967f
cck99 <161817893+cck99@users.noreply.github.com>
2024-08-27 20:04:56 +0800

    Time: 42 ms (28.01%), Space: 17.4 MB (42.7%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -1,38 +1,40 @@
+"""
+# Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = x
+        self.val = int(x)
         self.next = next
         self.random = random
+"""
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-        
-        # 第一步：拷贝每个节点并插入到原节点后面
+            
         cur = head
         while cur:
-            new_node = Node(cur.val, cur.next, None)
+            new_node = Node(cur.val,cur.next,None)
             cur.next = new_node
             cur = new_node.next
-        
-        # 第二步：设置拷贝节点的 random 指针
+
+
         cur = head
         while cur:
             if cur.random:
                 cur.next.random = cur.random.next
             cur = cur.next.next
-        
-        # 第三步：将两个链表分离
+
         cur = head
         copy_head = head.next
         copy_cur = copy_head
-        
+
         while cur:
-            cur.next = cur.next.next  # 修正：将原链表的 next 指针指向下一个原节点
+            cur.next = cur.next.next
             if copy_cur.next:
-                copy_cur.next = copy_cur.next.next  # 修正：将拷贝链表的 next 指针指向下一个拷贝节点
+                copy_cur.next = copy_cur.next.next
             cur = cur.next
             copy_cur = copy_cur.next
-        
+
         return copy_head
+        
